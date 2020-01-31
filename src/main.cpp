10d3b0d9a545a865f10fda4018ee80b2711d56b4
Kévin Le Torc'h <kev29lt@gmail.com>
2020-01-31 14:32:06 +0100

    Base Stuff V2
    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,54 @@
-#include <iostream>
+#include <string>
 
-int main(){
-    return 0;
+#include <mapnik/agg_renderer.hpp>
+#include <mapnik/datasource_cache.hpp>
+#include <mapnik/image.hpp>
+#include <mapnik/image_util.hpp>
+#include <mapnik/load_map.hpp>
+#include <mapnik/map.hpp>
+#include <mapnik/plugin.hpp>
+
+#define isCharArrayNumeric(x) \
+  std::all_of(std::string(x).begin(), std::string(x).end(), ::isdigit)
+
+#define DEFAULT_DATA_SOURCES_PATH "/usr/lib/mapnik/3.0/input"
+#define DEFAULT_MAP_PATH "data/world/world.xml"
+#define DEFAULT_OUTPUT_PATH "our_world.png"
+#define DEFAULT_WIDTH 7020   // A1 300pp
+#define DEFAULT_HEIGHT 9930  // A1 300pp
+
+int main(int argc, char **argv) {
+  int width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT;
+  std::string dataSourcesPath = DEFAULT_DATA_SOURCES_PATH,
+              mapPath = DEFAULT_MAP_PATH, outputPath = DEFAULT_OUTPUT_PATH;
+
+  switch (argc) {
+    case 6:
+      dataSourcesPath = argv[5];
+    case 5:
+      if (isCharArrayNumeric(argv[4])) height = std::atoi(argv[4]);
+    case 4:
+      if (isCharArrayNumeric(argv[3])) width = std::atoi(argv[3]);
+      break;
+    case 3:
+      mapPath = argv[2];
+    case 2:
+      outputPath = argv[1];
+    default:
+      break;
+  }
+
+  mapnik::datasource_cache::instance().register_datasources(dataSourcesPath);
+
+  mapnik::Map map{width, height};
+  mapnik::load_map(map, mapPath);
+  map.zoom_all();
+
+  mapnik::image_rgba8 image{width, height};
+  mapnik::agg_renderer<mapnik::image_rgba8> renderer{map, image};
+  renderer.apply();
+
+  mapnik::save_to_file(image, outputPath);
+
+  return 0;
 }
