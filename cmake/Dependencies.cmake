0af8fb5aa9665d64c23e64e9fb8cad6a33071b43
BlivionIaG <kev29lt@gmail.com>
2020-04-30 00:44:01 +0200

    JSON fix

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -5,15 +5,15 @@ include_directories(SYSTEM ${PQXX_INCLUDE_DIRECTORIES})
 list(APPEND Dependencies_LINKER_LIBS ${PQXX_LIBRARIES})
 
 
-find_package(nlohmann_json 3.2.0 REQUIRED)
+find_package(nlohmann_json 3.2.0)
 IF (${nlohmann_json_FOUND})
     MESSAGE(STATUS "nlohmann_json found !")
 ELSE (${nlohmann_json_FOUND})
     set(JSON_BuildTests OFF CACHE INTERNAL "")
     execute_process(
-        COMMAND bash "-c" "wget https://github.com/nlohmann/json.git"
+        COMMAND bash "-c" "git clone https://github.com/nlohmann/json.git"
     )
-    add_subdirectory(json)
+    add_subdirectory(${PROJECT_SOURCE_DIR}/build/json)
 ENDIF (${nlohmann_json_FOUND})
 
 list(APPEND Dependencies_LINKER_LIBS nlohmann_json::nlohmann_json)
