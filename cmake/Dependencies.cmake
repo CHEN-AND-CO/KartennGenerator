3d6094941604f78e4fe590bd822766ed93468e02
Kévin Le Torc'h <kev29lt@gmail.com>
2020-04-29 21:48:18 +0200

    import fix

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,7 +4,18 @@ find_package(PQXX REQUIRED)
 include_directories(SYSTEM ${PQXX_INCLUDE_DIRECTORIES})
 list(APPEND Dependencies_LINKER_LIBS ${PQXX_LIBRARIES})
 
-find_package(nlohmann_json REQUIRED)
+
+find_package(nlohmann_json 3.2.0 REQUIRED)
+IF (${nlohmann_json_FOUND})
+    MESSAGE(STATUS "nlohmann_json found !")
+ELSE (${nlohmann_json_FOUND})
+    set(JSON_BuildTests OFF CACHE INTERNAL "")
+    execute_process(
+        COMMAND bash "-c" "wget https://github.com/nlohmann/json.git"
+    )
+    add_subdirectory(json)
+ENDIF (${nlohmann_json_FOUND})
+
 list(APPEND Dependencies_LINKER_LIBS nlohmann_json::nlohmann_json)
 
 find_package(Mapnik REQUIRED)
