e6ffb52233865cce74503edb93f20fc4d757846b
BlivionIaG <kev29lt@gmail.com>
2020-04-30 18:22:03 +0200

    final json fix

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,7 +4,6 @@ find_package(PQXX REQUIRED)
 include_directories(SYSTEM ${PQXX_INCLUDE_DIRECTORIES})
 list(APPEND Dependencies_LINKER_LIBS ${PQXX_LIBRARIES})
 
-
 find_package(nlohmann_json 3.2.0)
 IF (${nlohmann_json_FOUND})
     MESSAGE(STATUS "nlohmann_json found !")
@@ -13,6 +12,9 @@ ELSE (${nlohmann_json_FOUND})
     execute_process(
         COMMAND bash "-c" "git clone https://github.com/nlohmann/json.git"
     )
+    while(NOT EXISTS ${PROJECT_SOURCE_DIR}/build/json)
+        ctest_sleep(1)
+    endwhile()
     add_subdirectory(${PROJECT_SOURCE_DIR}/build/json)
 ENDIF (${nlohmann_json_FOUND})
 
