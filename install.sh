8c13e8a9668fa5da8d6d396312d006ae6d03c521
Kévin Le Torc'h <kev29lt@gmail.com>
2020-05-03 21:46:00 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-sudo apt install -y mapnik* libpqxx-dev libmapnik-dev nlohmann-json3-dev
+set -e
+
+function dependencies(){
+    echo 'Installs C++ dependencies and tools'
+    sudo apt -y install gcc g++ cmake mapnik mapnik-doc mapnik-reference mapnik-utils libpqxx-dev libmapnik-dev nlohmann-json3-dev
+}
+
+function build(){
+    echo 'Configuring...'
+    mkdir -p build
+    cd build
+    cmake ..
+    echo 'Building...'
+    make -j
+}
+
+dependencies()
+build()
